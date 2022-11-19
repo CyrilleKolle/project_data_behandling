7f948e9f3839b69856d82f1ee91c3c6c4eef2b40
CyrilleKolle <ngidekollecyrille@gmail.com>
2022-11-19 17:30:03 +0100

    Added a dashboard template with some grapths

--- Dashboard/layout.py
+++ Dashboard/layout.py
@@ -2,37 +2,25 @@ from dash import html, dcc, Dash
 import dash_bootstrap_components as dbc
 import pandas as pd
 
+
 class Layout:
-    def __init__(self, data_dict: pd.DataFrame) -> None:
-        self._data_dict = data_dict
+    def __init__(self, symbol_dict: dict) -> None:
+        self._symbol_dict = symbol_dict
 
         self._olympics_options_dropdown = [
-            {"label": name, "value": symbol} for symbol, name in data_dict.items()
+            {"label": name, "value": symbol} for symbol, name in symbol_dict.items()
         ]
-
-        self._ohlc_options = [
-            {"label": option, "value": option}
-            for option in ("open", "high", "low", "close")
-        ]
-
-        self._slider_marks = {
-            i: mark
-            for i, mark in enumerate(
-                ["1 day", "1 week", "1 month", "3 months", "1 year", "5 year", "Max"]
-            )
-        }
 
     def layout(self):
         return dbc.Container(
             [
-                dbc.Card(
-                    dbc.CardBody(html.H1("Vive La France")), className="mt-3"
-                ),
+                dbc.Card(dbc.CardBody(html.H1("Vive La France")), className="mt-3"),
+                dbc.Card(),
                 dbc.Row(
                     className="mt-4",
                     children=[
                         dbc.Col(
-                            html.P("Most dominated sports by France"),
+                            html.Header("Athlete Events"),
                             className="mt-1",
                             xs=12,
                             sm=12,
@@ -42,9 +30,9 @@ class Layout:
                         ),
                         dbc.Col(
                             dcc.Dropdown(
-                                id="data-dropdown",
+                                id="olympic-dropdown",
                                 options=self._olympics_options_dropdown,
-                                value="france_most",
+                                value="athlete",
                             ),
                             xs=12,
                             sm=12,
@@ -55,20 +43,42 @@ class Layout:
                     ],
                 ),
                 dbc.Row(
-                    [
-                        dbc.Col(
+                    className="mt-4",
+                    children=[
+                        html.Section(
                             [
-                                dcc.Graph(id="france-max-graph"),
-                     
-                            ],
-                            xs="12",
-                            sm="12",
-                            md="12",
-                            lg={"size": 6},
-                            xl="6",
-                        ),
-                       
-                    ]
+                                dbc.Row(
+                                    children=[
+                                        dbc.Col(
+                                            [
+                                                dcc.Graph(id="france-max-graph"),
+                                            ],
+                                            xs="12",
+                                            sm="12",
+                                            md="12",
+                                            lg={"size": 6},
+                                            xl="6",
+                                        ),
+                                        dbc.Col(
+                                            [
+                                                dcc.Graph(id="france-all-ages"),
+                                            ],
+                                            xs="12",
+                                            sm="12",
+                                            md="12",
+                                            lg={"size": 6},
+                                            xl="6",
+                                        ),
+                                    ]
+                                ),
+                                dbc.Row(
+                                    [
+                                        dcc.Graph(id="france-medal-distribution"),
+                                    ]
+                                ),
+                            ]
+                        )
+                    ],
                 ),
                 # storing intermediate value on clients browser in order to share between several callbacks
                 dcc.Store(id="filtered-df"),
